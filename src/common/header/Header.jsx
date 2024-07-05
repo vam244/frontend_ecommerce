0926abbb7fb6bcbd600e0783a64286b95bcbfcd2
vamsi <vamsibalivada25@gmail.com>
2024-07-05 17:59:14 +0530

    updated css of nav and productpage

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,15 +1,14 @@
 import React from "react"
 import "./Header.css"
-// import Head from "./Head"
 import Search from "./Search"
-import Navbar from "./Navbar"
+
 
 const Header = ({ setsearch_product ,userData}) => {
   return (
     <>
-      {/* <Head /> */}
+      
       <Search setsearch_product ={setsearch_product } userData={userData} />
-      <Navbar />
+      
     </>
   )
 }
--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,45 +1,51 @@
-import React, { useState } from "react"
-import logo from "./logo_v.png"
-import { Link } from "react-router-dom"
+import React from "react";
+import logo from "./logo_v.png";
+import { Link } from "react-router-dom";
+import "./Search.css"; // Import the CSS file
 
-const Search = ({setsearch_product,userData }) => {
+const Search = ({ setsearch_product, userData }) => {
   // fixed Header
-  window.addEventListener("scroll", function () {
-    const search = document.querySelector(".search")
-    search.classList.toggle("active", window.scrollY > 100)
-  })
- 
- 
+  // window.addEventListener("scroll", function () {
+  //   const search = document.querySelector(".search");
+  //   search.classList.toggle("active", window.scrollY > 100);
+  // });
 
-  
   return (
     <>
-      <section className='search'>
-        <div className='container c_flex'>
-          <div className='logo width '>
-            <img src={logo} style={{width:50}} alt='' />
+      <section className="search">
+        <div className="container c_flex">
+          <div className="logo width">
+            <img src={logo} alt="" />
           </div>
 
-          <div className='search-box f_flex'style={{width:"50%",marginRight:60}}>
-            <input  type='text' placeholder='Search and hit enter...'onChange={(e) => setsearch_product(e.target.value)} /> 
-             {/* <i className='fa fa-lens'></i> */}
-            {/* <i className='fa fa-search' ></i> */}
+          <div className="search-box f_flex">
+            <input
+              type="text"
+              placeholder="Search and hit enter..."
+              onChange={(e) => setsearch_product(e.target.value)}
+            />
           </div>
 
-          <div className='icon f_flex width'>
-          
-            { !localStorage.getItem('token')? <Link to='/login'><i className='fa fa-user icon-circle'></i></Link> :<Link to='/login'><button style={{fontSize:"1rem"}}>{userData.username}|(logout)</button></Link>}
-            {/* <div className='cart'> */}
-              <Link to='/cart'>
-                <i className='fa fa-shopping-bag icon-circle'></i>
-                {/* <span>{CartItem.length === 0 ? "" : CartItem.length}</span> */}
+          <div className="icon f_flex width">
+            {!localStorage.getItem("token") ? (
+              <Link to="/login">
+                <i className="fa fa-user icon-circle"></i>
               </Link>
-            {/* </div> */}
+            ) : (
+              <Link to="/login">
+                <button>{userData.username}</button>
+              </Link>
+            )}
+            <Link to="/cart">
+              <i className="fa fa-shopping-bag icon-circle"></i>
+            </Link>
           </div>
         </div>
       </section>
+      <Link to='/' >Home</Link>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
+
